fe760d980675387c2eaf8b995a483fc76091234f
buddhika-jay <buddhika.12@cse.mrt.ac.lk>
2015-09-09 13:41:18 +0530

    Relationship Created

---
+++ src/AppBundle/Controller/VideoController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: buddhika
+ * Date: 9/9/15
+ * Time: 11:39 AM
+ */
+
+namespace AppBundle\Controller;
+
+use Siplo\MediaBundle\Controller\VideoController as BaseController;
+
+class VideoController extends BaseController
+{
+
+}
