8372c429e0024e63c5e747bf5a1b2f737415e97c
buddhika-jay <buddhika.12@cse.mrt.ac.lk>
2015-09-13 13:26:24 +0530

    Controllers restored to old way

--- src/Siplo/MediaBundle/Controller/PhotoController.php
+++ src/Siplo/MediaBundle/Controller/PhotoController.php
@@ -2,50 +2,39 @@
 
 namespace Siplo\MediaBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Siplo\MediaBundle\Entity\Photo;
-use Siplo\MediaBundle\Form\PhotoType;
+use Symfony\Component\HttpFoundation\Request;
+
 use Siplo\MediaBundle\Form\Type\PhotoUploaderType;
 use Siplo\MediaBundle\Form\Model\PhotoUpload;
 
-/**
- * Photo controller.
- *
- * @Route("/photo")
- */
 class PhotoController extends Controller
 {
-
     /**
-     * Lists all Photo entities.
+     * @Route("/photo/upload")
      *
-     * @Route("/", name="photo")
-     * @Method("GET")
-     * @Template()
      */
-    public function indexAction()
+    public function photoUploadAction()
     {
-        $em = $this->getDoctrine()->getManager();
+        $uploader = new PhotoUpload();
+        $form = $this->createForm(new PhotoUploaderType(), $uploader, array(
+            'action' => '/photo/upload/save',
+        ));
 
-        $entities = $em->getRepository('SiploMediaBundle:Photo')->findAll();
-
-        return array(
-            'entities' => $entities,
+        return $this->render(
+            'SiploMediaBundle::form.html.twig',
+            array('form' => $form->createView())
         );
     }
 
+
     /**
-     * Creates a new Photo entity.
+     * @Route("/photo/upload/save")
      *
-     * @Route("/", name="photo_create")
-     * @Method("POST")
-     * @Template("SiploMediaBundle:Photo:new.html.twig")
      */
-    public function createAction(Request $request)
+    public function saveAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
 
@@ -61,6 +50,7 @@ class PhotoController extends Controller
 
             $helper = $this->container->get('vich_uploader.templating.helper.uploader_helper');
             $path = $helper->asset($uploader->getPhoto(), 'photo');
+//            return $this->redirectToRoute('play');
             return $this->render('SiploMediaBundle::videoplayer.html.twig',array(
                 'path' => $path));
         }
@@ -69,172 +59,5 @@ class PhotoController extends Controller
             'SiploMediaBundle::form.html.twig',
             array('form' => $form->createView())
         );
-    }
-
-
-    /**
-     * Displays a form to create a new Photo entity.
-     *
-     * @Route("/new", name="photo_new")
-     * @Method("GET")
-     * @Template()
-     */
-    public function newAction()
-    {
-        $uploader = new PhotoUpload();
-        $form = $this->createForm(new PhotoUploaderType(), $uploader, array(
-            'action' => '/photo/',
-        ));
-
-        return $this->render(
-            'SiploMediaBundle::form.html.twig',
-            array('form' => $form->createView())
-        );
-    }
-
-    /**
-     * Finds and displays a Photo entity.
-     *
-     * @Route("/{id}", name="photo_show")
-     * @Method("GET")
-     * @Template()
-     */
-    public function showAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('SiploMediaBundle:Photo')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Photo entity.');
-        }
-
-        $deleteForm = $this->createDeleteForm($id);
-
-        return array(
-            'entity'      => $entity,
-            'delete_form' => $deleteForm->createView(),
-        );
-    }
-
-    /**
-     * Displays a form to edit an existing Photo entity.
-     *
-     * @Route("/{id}/edit", name="photo_edit")
-     * @Method("GET")
-     * @Template()
-     */
-    public function editAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('SiploMediaBundle:Photo')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Photo entity.');
-        }
-
-        $editForm = $this->createEditForm($entity);
-        $deleteForm = $this->createDeleteForm($id);
-
-        return array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        );
-    }
-
-    /**
-    * Creates a form to edit a Photo entity.
-    *
-    * @param Photo $entity The entity
-    *
-    * @return \Symfony\Component\Form\Form The form
-    */
-    private function createEditForm(Photo $entity)
-    {
-        $form = $this->createForm(new PhotoType(), $entity, array(
-            'action' => $this->generateUrl('photo_update', array('id' => $entity->getId())),
-            'method' => 'PUT',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Update'));
-
-        return $form;
-    }
-    /**
-     * Edits an existing Photo entity.
-     *
-     * @Route("/{id}", name="photo_update")
-     * @Method("PUT")
-     * @Template("SiploMediaBundle:Photo:edit.html.twig")
-     */
-    public function updateAction(Request $request, $id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('SiploMediaBundle:Photo')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Photo entity.');
-        }
-
-        $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createEditForm($entity);
-        $editForm->handleRequest($request);
-
-        if ($editForm->isValid()) {
-            $em->flush();
-
-            return $this->redirect($this->generateUrl('photo_edit', array('id' => $id)));
-        }
-
-        return array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        );
-    }
-    /**
-     * Deletes a Photo entity.
-     *
-     * @Route("/{id}", name="photo_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, $id)
-    {
-        $form = $this->createDeleteForm($id);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $entity = $em->getRepository('SiploMediaBundle:Photo')->find($id);
-
-            if (!$entity) {
-                throw $this->createNotFoundException('Unable to find Photo entity.');
-            }
-
-            $em->remove($entity);
-            $em->flush();
-        }
-
-        return $this->redirect($this->generateUrl('photo'));
-    }
-
-    /**
-     * Creates a form to delete a Photo entity by id.
-     *
-     * @param mixed $id The entity id
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm($id)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('photo_delete', array('id' => $id)))
-            ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete'))
-            ->getForm()
-        ;
     }
 }
