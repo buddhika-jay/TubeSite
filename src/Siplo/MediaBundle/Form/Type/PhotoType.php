1db3a3c49c819d5d1900b56196f0bfe74169fb28
buddhika-jay <buddhika.12@cse.mrt.ac.lk>
2015-09-15 22:05:15 +0530

    Asela's Home page not working. Pushing again

--- src/Siplo/MediaBundle/Form/Type/PhotoType.php
+++ src/Siplo/MediaBundle/Form/Type/PhotoType.php
@@ -11,7 +11,11 @@ namespace Siplo\MediaBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Validator\Constraints\Country;
 
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 
 class PhotoType extends AbstractType
@@ -32,6 +36,40 @@ class PhotoType extends AbstractType
             'class' => 'SiploMediaBundle:Category',
             'choice_label' => 'title',
         ));
+
+        $formModifier = function (FormInterface $form, Country $country = null) {
+            $categories = null === $country ? array() : $country->getCategories();
+
+            $form->add('category', 'entity', array(
+                'class'       => 'SiploMediaBundle:Category',
+                'placeholder' => '',
+                'choices'     => $categories,
+            ));
+        };
+
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            function (FormEvent $event) use ($formModifier) {
+                // this would be your entity, i.e. SportMeetup
+                $data = $event->getData();
+
+                $formModifier($event->getForm(), $data->getCountry());
+            }
+        );
+
+        $builder->get('country')->addEventListener(
+            FormEvents::POST_SUBMIT,
+            function (FormEvent $event) use ($formModifier) {
+                // It's important here to fetch $event->getForm()->getData(), as
+                // $event->getData() will get you the client data (that is, the ID)
+                $country = $event->getForm()->getData();
+
+                // since we've added the listener to the child, we'll have to pass on
+                // the parent to the callback functions!
+                $formModifier($event->getForm()->getParent(), $country);
+            }
+        );
+
         $builder->add('upload', 'submit');
     }
 
