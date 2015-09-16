d2746de21a206e19e1662010be88adce1f486d27
AselaDarshan <aseladarshan@gmail.com>
2015-09-16 23:10:56 +0530

    edit layout of photos page

--- src/Siplo/MediaBundle/Controller/SCategoryController.php
+++ src/Siplo/MediaBundle/Controller/SCategoryController.php
@@ -13,45 +13,5 @@ class SCategoryController extends Controller
 {
 
 
-    /**
-     * @Route("/{country}/{category}",requirements={
-     *     "country": "^[A-Z]{2}","category":"^[A-Z]{2}"
-     * }))
-     *
-     */
-    public function viewSCategoryAction($country,$category)
-    {
-        $countryEntity = $this->getDoctrine()
-            ->getRepository('SiploMediaBundle:Country')->findOneByCode($country);
 
-        $categoryEntity = $this->getDoctrine()
-            ->getRepository('SiploMediaBundle:Category')->findOneByCode($category);
-
-        $subCategories = $this->getDoctrine()
-            ->getRepository('SiploMediaBundle:SubCategory')->findAll();
-
-        if (!$subCategories) {
-            $this->render('AppBundle::emptycontent.html.twig');
-        }
-        $videos = $this->getDoctrine()
-            ->getRepository('SiploMediaBundle:Video')->findBy(
-                array('country'=>$countryEntity->getId(),'category'=>$categoryEntity->getId()),
-                array('rating' => 'DESC')
-            );
-        if (!$videos) {
-            return $this->render('AppBundle::emptycontent.html.twig'
-            );
-        }
-        return $this->render('AppBundle::subcategories.html.twig', array(
-            'subCategories' => $subCategories, 'country' => $countryEntity,'category'=>$categoryEntity,'videos' => $videos));
-
-    }
-    /**
-     * @Route("/more/{id}")
-     *
-     */
-    public function moreDetailsAction($id){
-        return $this->render('AppBundle::detailspage.html.twig');
-
-    }
 }
